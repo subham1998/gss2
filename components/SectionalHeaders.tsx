ecf3dca1c6cb258a648e104756d5b8750e8eea17
Subham <subhampanigrahi2931@gmail.com>
2023-09-19 23:46:47 +0530

    feat: more sections

--- components/SectionalHeaders.tsx
+++ components/SectionalHeaders.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from './SectionalHeader.module.scss'
 
-export default function SectionalHeader({image, title, link}: {image: any, title: string, link: string}) {
+export default function SectionalHeader({ image, title, link }: { image: any, title: string, link: string }) {
     return <>
-      <Link href={link}>
-                <h5 className={`${styles.header}`}>
-                    <Image src={image} alt="more-news" width={32} height={32} />
-                    <div className={`border-bottom border-2 text-dark ${styles['header-name']}`}>
-                        {title}
-                    </div>
-                </h5>
-            </Link>      
+        <Link href={link}>
+            <h5 className={`${styles.header}`}>
+                <Image src={image} alt="more-news" width={32} height={32} />
+                <div className={`border-bottom border-2 text-dark fw-bold ${styles['header-name']}`}>
+                    {title.toUpperCase()}
+                </div>
+            </h5>
+        </Link>
     </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import MoreNews from '@/components/MoreNews'
 import TuneIn from '@/components/TuneIn'
 import MediaCarousel from '@/components/MediaCarousel'
 import Actions from '@/components/Actions'
+import GovernanceCarousel from '@/components/GovernanceCarousel'
+import GlobalRecognition from '@/components/GlobalRecognition'
+import Summits from '@/components/Summits'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +22,15 @@ export default function Home() {
         <MoreNews></MoreNews>
         <MediaCarousel></MediaCarousel>
         <Actions></Actions>
+        <GovernanceCarousel></GovernanceCarousel>
+        <div className='row'>
+          <div className='col-lg-6 col-md-12'>
+            <GlobalRecognition></GlobalRecognition>
+          </div>
+          <div className='col-lg-6 col-md-12'>
+            <Summits></Summits>
+          </div>
+        </div>
       </div>
     </>
   )
